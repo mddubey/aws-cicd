4b1e77c546d1d011405788ed0934e08131180a87
MD Dubey <mddubey409@gmail.com>
2017-07-08 14:14:01 +0530

    Commiting failing test

---
+++ src/test/java/org/tw/cicd/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package org.tw.cicd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest {
+    @Test
+    public void helloWorldTest() throws Exception {
+        HelloController helloController = new HelloController();
+        assertEquals("Hell World", helloController.helloWorld());
+    }
+}
